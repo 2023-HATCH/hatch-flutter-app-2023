95b6944a5f280758112589b41fb6028f381f5f4e
devMooon <anstjdus0702@duksung.ac.kr>
2023-06-15 16:27:30 +0900

    :sparkles: [feat]page scroll(HH-176)

--- lib/ui/screen/home_screen.dart
+++ lib/ui/screen/home_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final _conrtoller = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.yellow,
-      body: Text(
-        "home",
-        style: TextStyle(color: Colors.white, fontSize: 50),
+    return Scaffold(
+      body: PageView(
+        controller: _conrtoller,
+        scrollDirection: Axis.vertical,
+        children: [
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+        ],
       ),
     );
   }
--- lib/ui/screen/main_screen.dart
+++ lib/ui/screen/main_screen.dart
@@ -18,7 +18,7 @@ class _MainScreenState extends State<MainScreen> {
   int _bottomNavIndex = 0;
 
   final List<Widget> _screens = <Widget>[
-    const HomeScreen(),
+    HomeScreen(),
     const ProfileScreen(),
   ];
 
