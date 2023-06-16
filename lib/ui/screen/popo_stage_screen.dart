fffe076ee5fe5f0a5cff9d165fe4b7b2e10f8428
minwachya <minwachya@gmail.com>
2023-06-16 22:32:19 +0900

    :sparkles: [feat] 포포 스테이지: 사용자 3명 모이면 대기 화면에서 캐치 화면으로 이동 타이머로 임시 구현

--- lib/ui/screen/popo_stage_screen.dart
+++ lib/ui/screen/popo_stage_screen.dart
@@ -1,9 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pocket_pose/ui/view/popo_catch_view.dart';
 import 'package:pocket_pose/ui/view/popo_wait_view.dart';
 
-class PoPoStageScreen extends StatelessWidget {
+class PoPoStageScreen extends StatefulWidget {
   const PoPoStageScreen({super.key});
+
+  @override
+  State<PoPoStageScreen> createState() => _PoPoStageScreenState();
+}
+
+class _PoPoStageScreenState extends State<PoPoStageScreen> {
+  int _userCount = 1;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _startTimer();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_userCount == 3) {
+        _stopTimer();
+      } else {
+        setState(() {
+          _userCount++;
+        });
+      }
+    });
+  }
+
+  void _stopTimer() {
+    _timer.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,48 +49,48 @@ class PoPoStageScreen extends StatelessWidget {
         ),
       ),
       child: Scaffold(
-        extendBodyBehindAppBar: true,
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "PoPo 스테이지",
-            style: TextStyle(fontSize: 18),
-          ),
+          extendBodyBehindAppBar: true,
           backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          leading: IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              'assets/icons/ic_home.svg',
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              "PoPo 스테이지",
+              style: TextStyle(fontSize: 18),
             ),
-          ),
-          actions: [
-            OutlinedButton.icon(
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            leading: IconButton(
               onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                minimumSize: Size.zero,
-                // padding: const EdgeInsets.fromLTRB(11, 7, 11, 7),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                side: const BorderSide(
-                  color: Colors.white,
-                  width: 1.0,
-                ),
-              ),
               icon: SvgPicture.asset(
-                'assets/icons/ic_users.svg',
-              ),
-              label: const Text(
-                '125',
-                style: TextStyle(color: Colors.white),
+                'assets/icons/ic_home.svg',
               ),
             ),
-          ],
-        ),
-        body: const PoPoWaitView(),
-      ),
+            actions: [
+              OutlinedButton.icon(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  minimumSize: Size.zero,
+                  // padding: const EdgeInsets.fromLTRB(11, 7, 11, 7),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  side: const BorderSide(
+                    color: Colors.white,
+                    width: 1.0,
+                  ),
+                ),
+                icon: SvgPicture.asset(
+                  'assets/icons/ic_users.svg',
+                ),
+                label: Text(
+                  '$_userCount',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          body:
+              (_userCount >= 3) ? const PoPoCatchView() : const PoPoWaitView()),
     );
   }
 }
