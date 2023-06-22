91506e5315035fd977bc5c6c60c5c0880ab0238a
devMooon <anstjdus0702@duksung.ac.kr>
2023-06-22 20:18:52 +0900

    :art: [ui]사용자 프로필 페이지 앱바 구현(HH-218)

--- lib/ui/screen/profile_screen.dart
+++ lib/ui/screen/profile_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pocket_pose/config/app_color.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pocket_pose/domain/provider/auth_provider.dart';
 import 'package:pocket_pose/ui/widget/not_login_widget.dart';
 import 'package:provider/provider.dart';
@@ -20,8 +20,27 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
     return authProvider.accessToken != null
         //토큰이 존재할 경우
-        ? const Center(child: Text("로그인이 완료 된 사용자 프로필"))
+        ? Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.transparent, //appBar 투명색
+              elevation: 0.0, //appBar 그림자 농도 설정 (값 0으로 제거)
+              actions: [
+                Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 14, 0),
+                    child:
+                        SvgPicture.asset('assets/icons/ic_profile_edit.svg')),
+                Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 14, 0),
+                    child: SvgPicture.asset(
+                        'assets/icons/ic_profile_setting.svg')),
+              ],
+            ),
+            extendBodyBehindAppBar: true, //body 위에 appbar
+            body: const Center(child: Text("로그인이 완료 된 사용자 프로필")))
+
         //토큰이 존재하지 않는 경우
         : const NotLoginWidget();
   }
 }
+
+//Center(child: Text("로그인이 완료 된 사용자 프로필"))
