512760e0365226ef5cedf8433e350cf5e29ba9bb
devMooon <anstjdus0702@duksung.ac.kr>
2023-06-23 01:50:48 +0900

    :art: [ui] 프로필 UI 구현. tabBar 없으면 기능 잘 됨 (HH-218)

--- lib/ui/screen/profile_screen.dart
+++ lib/ui/screen/profile_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:pocket_pose/config/app_color.dart';
 import 'package:pocket_pose/data/local/provider/auth_provider.dart';
 import 'package:pocket_pose/ui/widget/profile/profile_infomation_widget.dart';
 
@@ -14,8 +13,6 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen>
     with SingleTickerProviderStateMixin {
   late AuthProvider authProvider;
-  late TabController _tabController;
-  late Color selectedLineColor;
 
   final List<String> _videoImagePath = [
     "profile_video_0",
@@ -67,155 +64,90 @@ class _ProfileScreenState extends State<ProfileScreen>
     "profile_video_10",
     "profile_video_11",
   ];
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 2, vsync: this);
-    selectedLineColor = AppColor.purpleColor; // 선택된 탭의 줄 색상
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                (context, index) => Container(
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Container(
-                                  margin:
-                                      const EdgeInsets.fromLTRB(0, 14, 14, 0),
-                                  child: SvgPicture.asset(
-                                      'assets/icons/ic_profile_edit.svg')),
-                              Container(
-                                  margin:
-                                      const EdgeInsets.fromLTRB(0, 14, 14, 0),
-                                  child: SvgPicture.asset(
-                                      'assets/icons/ic_profile_setting.svg')),
-                            ],
-                          ),
-                          ProfileInfomationWidget(),
-                        ],
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  (context, index) => Container(
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                    margin:
+                                        const EdgeInsets.fromLTRB(0, 14, 14, 0),
+                                    child: SvgPicture.asset(
+                                        'assets/icons/ic_profile_edit.svg')),
+                                Container(
+                                    margin:
+                                        const EdgeInsets.fromLTRB(0, 14, 14, 0),
+                                    child: SvgPicture.asset(
+                                        'assets/icons/ic_profile_setting.svg')),
+                              ],
+                            ),
+                            ProfileInfomationWidget(),
+                          ],
+                        ),
+                      ),
+                  childCount: 1),
+            ),
+            const SliverAppBar(
+              pinned: true,
+              backgroundColor: Colors.white,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Icon(Icons.dashboard),
+                  Icon(Icons.tv),
+                  Icon(Icons.person),
+                ],
+              ),
+            ),
+            // SliverAnimatedList(
+            //   itemBuilder: (_, index, ___) {
+            //     return ListTile(
+            //       title: Text(index.toString()),
+            //     );
+            //   },
+            //   initialItemCount: 100,
+            // ),
+            SliverAnimatedGrid(
+              initialItemCount: _videoImagePath.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: MediaQuery.of(context).size.width /
+                    (MediaQuery.of(context).size.height),
+              ),
+              itemBuilder: (context, index, animation) {
+                return ScaleTransition(
+                  scale: animation,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 1,
                       ),
                     ),
-                childCount: 1),
-          ),
-          SliverAppBar(
-            pinned: true,
-            backgroundColor: Colors.white,
-            title: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Icon(Icons.dashboard),
-                Icon(Icons.tv),
-                Icon(Icons.person),
-              ],
-            ),
-            bottom: TabBar(
-              controller: _tabController,
-              indicatorColor: Colors.blue,
-              labelColor: Colors.blue,
-              unselectedLabelColor: Colors.black,
-              tabs: const [
-                Tab(text: 'Grid 1'),
-                Tab(text: 'Grid 2'),
-              ],
-            ),
-          ),
-          // SliverAnimatedList(
-          //   itemBuilder: (_, index, ___) {
-          //     return ListTile(
-          //       title: Text(index.toString()),
-          //     );
-          //   },
-          //   initialItemCount: 100,
-          // ),
-          SliverFillRemaining(
-            child: TabBarView(
-              controller: _tabController,
-              children: [
-                Builder(
-                  builder: (context) => CustomScrollView(
-                    slivers: [
-                      SliverGrid(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          childAspectRatio: MediaQuery.of(context).size.width /
-                              (MediaQuery.of(context).size.height - 200),
-                        ),
-                        delegate: SliverChildBuilderDelegate(
-                          (context, index) {
-                            return Container(
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 1,
-                                ),
-                              ),
-                              child: Image.asset(
-                                "assets/images/${_videoImagePath[index]}.png",
-                                fit: BoxFit.cover,
-                              ),
-                            );
-                          },
-                          childCount: _videoImagePath.length,
-                        ),
-                      ),
-                    ],
+                    child: Image.asset(
+                      "assets/images/${_videoImagePath[index]}.png",
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                Builder(
-                  builder: (context) => CustomScrollView(
-                    slivers: [
-                      SliverGrid(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          childAspectRatio: MediaQuery.of(context).size.width /
-                              (MediaQuery.of(context).size.height - 200),
-                        ),
-                        delegate: SliverChildBuilderDelegate(
-                          (context, index) {
-                            return Container(
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 1,
-                                ),
-                              ),
-                              child: Image.asset(
-                                "assets/images/${_videoImagePath[index]}.png",
-                                fit: BoxFit.cover,
-                              ),
-                            );
-                          },
-                          childCount: _videoImagePath.length,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                );
+              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ));
+    );
   }
-}
+
   // @override
   // Widget build(BuildContext context) {
   //   authProvider = Provider.of<AuthProvider>(context);
@@ -251,4 +183,4 @@ class _ProfileScreenState extends State<ProfileScreen>
   //       //토큰이 존재하지 않는 경우
   //       : const NotLoginWidget();
   // }
-
+}
