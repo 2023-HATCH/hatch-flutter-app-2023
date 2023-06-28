62b54a33759b7bbc022592e74a9be3dc55f68b03
devMooon <anstjdus0702@duksung.ac.kr>
2023-06-28 11:54:45 +0900

    :sparkles: [feat] 비워진 불꽃 아이콘 클릭 시 채워진 불꽃이 위로 올라가기 (HH-230)

---
+++ lib/ui/screen/profile/profile_edit_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ProfileEditScreen extends StatefulWidget {
+  const ProfileEditScreen({Key? key}) : super(key: key);
+
+  @override
+  _ProfileEditScreenState createState() => _ProfileEditScreenState();
+}
+
+class _ProfileEditScreenState extends State<ProfileEditScreen>
+    with SingleTickerProviderStateMixin {
+  List<Widget> selectWidgets = [];
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 3000),
+      vsync: this,
+    );
+
+    _animation = Tween(begin: 0.0, end: -200.0).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+
+    _animationController.addListener(() {
+      setState(() {});
+    });
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          selectWidgets.removeAt(0);
+        });
+      }
+    });
+  }
+
+  void _handleIconClick() {
+    setState(() {
+      selectWidgets.add(buildSelectWidget());
+    });
+    _animationController.forward(from: 0);
+  }
+
+  Widget buildSelectWidget() {
+    return Positioned(
+      bottom: 0,
+      right: 0,
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return Transform.translate(
+            offset: Offset(0, _animation.value),
+            child: child,
+          );
+        },
+        child: Image.asset(
+          'assets/icons/ic_popo_fire_select.png',
+          height: 50,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            right: 12,
+            child: InkWell(
+              highlightColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              onTap: _handleIconClick,
+              child: SvgPicture.asset('assets/icons/ic_popo_fire_unselect.svg'),
+            ),
+          ),
+          ...selectWidgets,
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+}
--- lib/ui/screen/profile/profile_screen.dart
+++ lib/ui/screen/profile/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pocket_pose/config/app_color.dart';
 import 'package:pocket_pose/data/remote/provider/auth_provider.dart';
+import 'package:pocket_pose/ui/screen/profile/profile_edit_screen.dart';
 import 'package:pocket_pose/ui/screen/profile/profile_setting_screen.dart';
 
 import 'package:pocket_pose/ui/widget/not_login_widget.dart';
@@ -87,10 +88,20 @@ class _ProfileScreenState extends State<ProfileScreen>
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Container(
-                            margin: const EdgeInsets.fromLTRB(0, 44, 14, 0),
-                            child: SvgPicture.asset(
-                                'assets/icons/ic_profile_edit.svg'),
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const ProfileEditScreen()),
+                              );
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.fromLTRB(0, 44, 14, 0),
+                              child: SvgPicture.asset(
+                                  'assets/icons/ic_profile_edit.svg'),
+                            ),
                           ),
                           InkWell(
                             onTap: () {
