1e9328fbc05d7c4bf9856fd30b93ead8a0038271
minWachya <minWachya@gmail.com>
2023-06-30 21:46:50 +0900

    :art: [ui] 스테이지 실시간 채팅 바에 불꽃 아이콘 + 기능 추가

--- lib/ui/screen/popo_stage_screen.dart
+++ lib/ui/screen/popo_stage_screen.dart
@@ -92,12 +92,18 @@ class PoPoStageScreen extends StatefulWidget {
   State<PoPoStageScreen> createState() => _PoPoStageScreenState();
 }
 
-class _PoPoStageScreenState extends State<PoPoStageScreen> {
+class _PoPoStageScreenState extends State<PoPoStageScreen>
+    with TickerProviderStateMixin {
   int _userCount = 1;
   int _count = 1;
   late Timer _timer;
   late VideoPlayProvider _videoPlayProvider;
   final TextEditingController _textController = TextEditingController();
+
+  List<Widget> selectWidgets = [];
+  final List<AnimationController> _animationControllers = [];
+  final List<Animation<Offset>> _animations = [];
+  bool isLeft = true;
 
   StageStage _stageStage = StageStage.waitState;
   @override
@@ -112,6 +118,9 @@ class _PoPoStageScreenState extends State<PoPoStageScreen> {
     AudioPlayerUtil().stop();
     if (widget.getIndex() == 0) {
       _videoPlayProvider.playVideo();
+    }
+    for (final animationController in _animationControllers) {
+      animationController.dispose();
     }
     super.dispose();
   }
@@ -202,6 +211,7 @@ class _PoPoStageScreenState extends State<PoPoStageScreen> {
                     right: 0,
                     child: buildInputArea(context),
                   ),
+                  ...selectWidgets,
                 ],
               ),
             )),
@@ -216,29 +226,48 @@ class _PoPoStageScreenState extends State<PoPoStageScreen> {
         child: Padding(
           padding:
               EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: Container(
-            padding: const EdgeInsets.only(left: 15),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(36),
-              color: Colors.transparent,
-              border: Border.all(
-                color: Colors.white,
-                width: 2.0,
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(14),
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 14),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(36),
+                      color: Colors.transparent,
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2.0,
+                      ),
+                    ),
+                    child: TextField(
+                      style: const TextStyle(color: Colors.white),
+                      controller: _textController,
+                      cursorColor: Colors.white,
+                      decoration: const InputDecoration(
+                        hintText: 'nickname(으)로 댓글 달기...',
+                        hintStyle: TextStyle(color: Colors.white70),
+                        labelStyle: TextStyle(color: Colors.white),
+                        border: InputBorder.none,
+                      ),
+                      textInputAction: TextInputAction.next,
+                    ),
+                  ),
+                ),
               ),
-            ),
-            child: TextField(
-              style: const TextStyle(color: Colors.white),
-              controller: _textController,
-              cursorColor: Colors.white,
-              decoration: const InputDecoration(
-                hintText: 'nickname(으)로 댓글 달기...',
-                hintStyle: TextStyle(color: Colors.white70),
-                labelStyle: TextStyle(color: Colors.white),
-                border: InputBorder.none,
+              InkWell(
+                highlightColor: Colors.transparent,
+                splashColor: Colors.transparent,
+                onTap: _handleIconClick,
+                child:
+                    SvgPicture.asset('assets/icons/ic_popo_fire_unselect.svg'),
               ),
-              textInputAction: TextInputAction.next,
-            ),
+              const SizedBox(
+                width: 14,
+              )
+            ],
           ),
         ),
       ),
@@ -374,5 +403,85 @@ class _PoPoStageScreenState extends State<PoPoStageScreen> {
       default:
         return const PoPoWaitView();
     }
+  }
+
+  void _handleIconClick() {
+    setState(() {
+      isLeft = !isLeft;
+
+      final animationController = AnimationController(
+        duration: const Duration(milliseconds: 4000),
+        vsync: this,
+      );
+
+      const beginOffset = Offset(0.0, 0.0);
+      final middleOffset =
+          isLeft ? const Offset(-8.0, -100.0) : const Offset(8.0, -100.0);
+      const endOffset = Offset(0.0, -200.0);
+
+      final animation = TweenSequence([
+        TweenSequenceItem(
+          tween: Tween<Offset>(begin: beginOffset, end: middleOffset),
+          weight: 0.5,
+        ),
+        TweenSequenceItem(
+          tween: Tween<Offset>(begin: middleOffset, end: endOffset),
+          weight: 0.5,
+        ),
+      ]).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeInOut,
+      ));
+      animationController.addListener(() {
+        setState(() {});
+      });
+
+      animationController.addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          setState(() {
+            selectWidgets.removeAt(0);
+            _animationControllers.removeAt(0);
+            _animations.removeAt(0);
+          });
+        }
+      });
+
+      selectWidgets.add(buildSelectWidget(
+        selectWidgets.length,
+        animationController,
+        animation,
+      ));
+      _animationControllers.add(animationController);
+      _animations.add(animation);
+    });
+
+    _animationControllers.last.forward(from: 0);
+  }
+
+  Widget buildSelectWidget(int index, AnimationController animationController,
+      Animation<Offset> animation) {
+    return Positioned(
+      bottom: 0,
+      right: 0,
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return Transform.translate(
+            offset: Offset(
+              animation.value.dx,
+              animation.value.dy,
+            ),
+            child: FadeTransition(
+              opacity: animationController.drive(Tween(begin: 1.0, end: 0.0)),
+              child: child,
+            ),
+          );
+        },
+        child: Image.asset(
+          'assets/icons/ic_popo_fire_select.png',
+          height: 50,
+        ),
+      ),
+    );
   }
 }
