0dbf0faeedcc35afce707bc812768181a07c6ed6
minWachya <minWachya@gmail.com>
2023-07-01 16:17:45 +0900

    :art: [ui] 스테이지 실시간 채팅 목록 아이템 ui 생성

--- lib/ui/widget/stage/stage_live_chat_list.widget.dart
+++ lib/ui/widget/stage/stage_live_chat_list.widget.dart
@@ -5,23 +5,62 @@ class StageLiveChatListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> entries = <String>['a', 'b', 'c', 'd'];
+    final List<String> entries = <String>[
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      '1',
+      '2',
+      '3'
+    ];
 
     return SafeArea(
       child: SingleChildScrollView(
         child: Container(
           color: Colors.black.withOpacity(0.3),
-          height: 200,
-          child: ListView.builder(
-              padding: const EdgeInsets.fromLTRB(8, 8, 8, 100),
+          height: 150,
+          child: ListView.separated(
+              padding: const EdgeInsets.all(14),
               itemCount: entries.length,
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 12);
+              },
               itemBuilder: (BuildContext context, int index) {
-                return SizedBox(
-                    child: Center(
-                        child: Text(
-                  '순서대로 ${entries[index]}',
-                  style: const TextStyle(color: Colors.white),
-                )));
+                return Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.asset(
+                        'assets/images/home_profile_1.jpg',
+                        width: 35,
+                        height: 35,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'nickname ${entries[index]}',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          'content ${entries[index]}',
+                          style: const TextStyle(color: Colors.white),
+                        )
+                      ],
+                    )
+                  ],
+                );
               }),
         ),
       ),
