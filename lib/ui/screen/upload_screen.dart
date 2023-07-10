fcafe9a00fe7900080e7d8e62b0762bf8e26f56e
minWachya <minWachya@gmail.com>
2023-07-10 20:06:10 +0900

    :recycle: [refactor] 업로드 화면 ui 코드 함수화

--- lib/ui/screen/upload_screen.dart
+++ lib/ui/screen/upload_screen.dart
@@ -32,35 +32,9 @@ class _UploadScreenState extends State<UploadScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          "미리보기",
-          style: TextStyle(
-              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        leading: TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text(
-            "취소",
-            style: TextStyle(color: Colors.grey, fontSize: 14),
-          ),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "업로드",
-              style: TextStyle(color: Colors.blue, fontSize: 14),
-            ),
-          )
-        ],
-      ),
-      body: Container(
+      resizeToAvoidBottomInset: false,
+      appBar: _buildAppBar(context),
+      body: SafeArea(
         child: Column(
           children: [
             Container(
@@ -79,94 +53,129 @@ class _UploadScreenState extends State<UploadScreen> {
                 },
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(18, 20, 18, 7),
-              child: Row(
-                children: [
-                  const Text(
-                    "제목",
-                    style: TextStyle(fontSize: 14, color: Colors.black),
-                  ),
-                  const SizedBox(
-                    width: 18,
-                  ),
-                  Expanded(
-                    child: Container(
-                      height: 40,
-                      width: double.infinity,
-                      padding: const EdgeInsets.only(left: 14),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 2.5,
-                        ),
-                      ),
-                      child: TextField(
-                        style:
-                            const TextStyle(color: Colors.black, fontSize: 14),
-                        controller: _titleController,
-                        cursorColor: Colors.grey,
-                        decoration: const InputDecoration(
-                          hintText: '포포와 함께 댄스 파티',
-                          hintStyle:
-                              TextStyle(color: Colors.grey, fontSize: 14),
-                          labelStyle: TextStyle(color: Colors.grey),
-                          border: InputBorder.none,
-                        ),
-                        textInputAction: TextInputAction.next,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(18, 7, 18, 20),
-              child: Row(
-                children: [
-                  const Text(
-                    "태그",
-                    style: TextStyle(fontSize: 14, color: Colors.black),
-                  ),
-                  const SizedBox(
-                    width: 18,
-                  ),
-                  Expanded(
-                    child: Container(
-                      height: 40,
-                      width: double.infinity,
-                      padding: const EdgeInsets.only(left: 14),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.transparent,
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 2.5,
-                        ),
-                      ),
-                      child: TextField(
-                        style:
-                            const TextStyle(color: Colors.black, fontSize: 14),
-                        controller: _tagController,
-                        cursorColor: Colors.grey,
-                        decoration: const InputDecoration(
-                          hintText: '#포포 #댄스챌린지',
-                          hintStyle:
-                              TextStyle(color: Colors.grey, fontSize: 14),
-                          labelStyle: TextStyle(color: Colors.grey),
-                          border: InputBorder.none,
-                        ),
-                        textInputAction: TextInputAction.next,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            _buildVideoInfoArea(),
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildVideoInfoArea() {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(18, 20, 18, 7),
+          child: Row(
+            children: [
+              const Text(
+                "제목",
+                style: TextStyle(fontSize: 14, color: Colors.black),
+              ),
+              const SizedBox(
+                width: 18,
+              ),
+              Expanded(
+                child: Container(
+                  height: 40,
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(left: 14),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 2.5,
+                    ),
+                  ),
+                  child: TextField(
+                    style: const TextStyle(color: Colors.black, fontSize: 14),
+                    controller: _titleController,
+                    cursorColor: Colors.grey,
+                    decoration: const InputDecoration(
+                      hintText: '포포와 함께 댄스 파티',
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 14),
+                      labelStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none,
+                    ),
+                    textInputAction: TextInputAction.next,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(18, 7, 18, 20),
+          child: Row(
+            children: [
+              const Text(
+                "태그",
+                style: TextStyle(fontSize: 14, color: Colors.black),
+              ),
+              const SizedBox(
+                width: 18,
+              ),
+              Expanded(
+                child: Container(
+                  height: 40,
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(left: 14),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.transparent,
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 2.5,
+                    ),
+                  ),
+                  child: TextField(
+                    style: const TextStyle(color: Colors.black, fontSize: 14),
+                    controller: _tagController,
+                    cursorColor: Colors.grey,
+                    decoration: const InputDecoration(
+                      hintText: '#포포 #댄스챌린지',
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 14),
+                      labelStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none,
+                    ),
+                    textInputAction: TextInputAction.next,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: const Text(
+        "미리보기",
+        style: TextStyle(
+            fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: TextButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Text(
+          "취소",
+          style: TextStyle(color: Colors.grey, fontSize: 14),
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            "업로드",
+            style: TextStyle(color: Colors.blue, fontSize: 14),
+          ),
+        )
+      ],
     );
   }
 }
