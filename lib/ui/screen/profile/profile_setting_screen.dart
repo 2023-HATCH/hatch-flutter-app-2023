4a9cb7c78bed0d48250b76d67e27b06f4a7c8d18
devMooon <anstjdus0702@duksung.ac.kr>
2023-07-17 11:17:15 +0900

    :art: [ui] 설정 페이지 appbar 개발 (HH-218)

--- lib/ui/screen/profile/profile_setting_screen.dart
+++ lib/ui/screen/profile/profile_setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_pose/config/app_color.dart';
 import 'package:pocket_pose/data/remote/provider/auth_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -22,6 +23,31 @@ class _ProfileSettingScreenState extends State<ProfileSettingScreen> {
     authProvider = Provider.of<AuthProvider>(context);
 
     return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            '설정 및 개인정보',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          leading: IconButton(
+            icon: Image.asset(
+              'assets/icons/ic_back.png',
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          elevation: 0,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(2.0),
+            child: Container(
+              height: 2.0,
+              color: AppColor.purpleColor,
+            ),
+          ),
+        ),
         body: Center(
             child: TextButton(
                 onPressed: () {
