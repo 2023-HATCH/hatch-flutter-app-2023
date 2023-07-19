85c63eb545f408ea7667176bfd1941bcf7ed9641
devMooon <anstjdus0702@duksung.ac.kr>
2023-07-19 13:20:50 +0900

    :art: [ui] 영상 제목 왼쪽 정렬 (HH-176)

--- lib/ui/video_viewer/video_user_info_frame.dart
+++ lib/ui/video_viewer/video_user_info_frame.dart
@@ -20,33 +20,35 @@ class VideoUserInfoFrame extends StatelessWidget {
         child: SizedBox(
           //color: Colors.green,
           width: 300,
-          child: Column(children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                Fluttertoast.showToast(msg: 'user 클릭');
-              },
-              child: Row(children: <Widget>[
-                ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: Image.asset(_videoPlayProvider.profiles[index],
-                        width: 35)),
-                const Padding(padding: EdgeInsets.only(left: 8)),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Fluttertoast.showToast(msg: 'user 클릭');
+                  },
+                  child: Row(children: <Widget>[
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.asset(_videoPlayProvider.profiles[index],
+                            width: 35)),
+                    const Padding(padding: EdgeInsets.only(left: 8)),
+                    Text(
+                      _videoPlayProvider.nicknames[index],
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ]),
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 8)),
                 Text(
-                  _videoPlayProvider.nicknames[index],
-                  style: const TextStyle(color: Colors.white),
+                  _videoPlayProvider.contents[index],
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ]),
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 8)),
-            Text(
-              _videoPlayProvider.contents[index],
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ]),
         ));
   }
 }
