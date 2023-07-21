5ad7c992a685df38121a657e16e0d20494568910
minWachya <minWachya@gmail.com>
2023-07-21 19:44:00 +0900

    :art: [ui] 채팅 목록 앱바 추가

--- lib/ui/screen/chat_list_screen.dart
+++ lib/ui/screen/chat_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pocket_pose/config/app_color.dart';
 import 'package:pocket_pose/data/local/provider/video_play_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -27,8 +29,37 @@ class _ChatListScreenState extends State<ChatListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.red,
+    return Scaffold(
+      appBar: _buildAppBar(context),
+      body: Column(children: [
+        Container(
+          color: AppColor.purpleColor,
+          height: 3,
+        ),
+      ]),
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: const Text(
+        "메시지",
+        style: TextStyle(
+            fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: TextButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: SvgPicture.asset(
+          'assets/icons/ic_left_purple.svg',
+          width: 11,
+          height: 19,
+        ),
+      ),
     );
   }
 }
