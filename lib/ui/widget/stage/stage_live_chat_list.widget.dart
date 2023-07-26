350293f9df254906ff17dc77b3207ae57bc9017e
minWachya <minWachya@gmail.com>
2023-07-26 13:24:42 +0900

    :art: [ui] 스테이지 실시간 채팅창 경계선 흐리도록 수정

--- lib/ui/widget/stage/stage_live_chat_list.widget.dart
+++ lib/ui/widget/stage/stage_live_chat_list.widget.dart
@@ -51,48 +51,68 @@ class _StageLiveChatListWidgetState extends State<StageLiveChatListWidget> {
   SingleChildScrollView _buildStageChatList(List<String> entries) {
     return SingleChildScrollView(
       child: Container(
-        color: Colors.black.withOpacity(0.3),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.transparent,
+              Colors.black.withOpacity(0.3),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: const [0.0001, 0.25],
+          ),
+        ),
         height: 150,
-        child: ListView.separated(
-            controller: _scrollController,
-            padding: const EdgeInsets.all(14),
-            itemCount: entries.length,
-            separatorBuilder: (context, index) {
-              return const SizedBox(height: 12);
-            },
-            itemBuilder: (BuildContext context, int index) {
-              return Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: Image.asset(
-                      'assets/images/home_profile_1.jpg',
-                      width: 35,
-                      height: 35,
+        child: ShaderMask(
+          shaderCallback: (Rect bounds) {
+            return LinearGradient(
+              begin: Alignment.center,
+              end: Alignment.topCenter,
+              colors: [Colors.white, Colors.white.withOpacity(0.02)],
+              stops: const [0.8, 1],
+            ).createShader(bounds);
+          },
+          child: ListView.separated(
+              controller: _scrollController,
+              padding: const EdgeInsets.all(14),
+              itemCount: entries.length,
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 12);
+              },
+              itemBuilder: (BuildContext context, int index) {
+                return Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.asset(
+                        'assets/images/home_profile_1.jpg',
+                        width: 35,
+                        height: 35,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'nickname ${entries[index]}',
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        'content ${entries[index]}',
-                        style: const TextStyle(color: Colors.white),
-                      )
-                    ],
-                  )
-                ],
-              );
-            }),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'nickname ${entries[index]}',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          'content ${entries[index]}',
+                          style: const TextStyle(color: Colors.white),
+                        )
+                      ],
+                    )
+                  ],
+                );
+              }),
+        ),
       ),
     );
   }
