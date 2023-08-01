c9568824f3869a6acb619520b69e55f55d548b6b
minWachya <minWachya@gmail.com>
2023-08-01 10:56:48 +0900

    :sparkles: [feat] 스테이지 실시간 채팅 응답 데이터 생성

---
+++ lib/data/entity/socket_response/talk_message_response.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'talk_message_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TalkMessageResponse _$TalkMessageResponseFromJson(Map<String, dynamic> json) =>
+    TalkMessageResponse(
+      content: json['content'] as String,
+      sender: ChatUserListItem.fromJson(json['sender'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$TalkMessageResponseToJson(
+        TalkMessageResponse instance) =>
+    <String, dynamic>{
+      'content': instance.content,
+      'sender': instance.sender,
+    };
