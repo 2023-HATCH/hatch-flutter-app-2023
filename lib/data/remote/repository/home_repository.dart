94d1a3cf6da18ef1190de1bf7b23bbab6cd99a4f
devMooon <anstjdus0702@duksung.ac.kr>
2023-08-06 01:09:17 +0900

    :sparkles: [feat] 홈 영상 조회에 토큰 추가 전송 (HH-176)

--- lib/data/remote/repository/home_repository.dart
+++ lib/data/remote/repository/home_repository.dart
@@ -1,15 +1,23 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:pocket_pose/config/api_url.dart';
 import 'package:pocket_pose/data/entity/request/home_videos_request.dart';
 import 'package:pocket_pose/data/entity/response/home_videos_response.dart';
 import 'package:pocket_pose/domain/entity/video_data.dart';
 
+const _storage = FlutterSecureStorage();
+const _accessTokenKey = 'kakaoAccessToken';
+const _refreshTokenKey = 'kakaoRefreshToken';
+
 class HomeRepository {
   Future<HomeVideosResponse> getVideos(
       HomeVideosRequest homeVideosRequest) async {
+    final accessToken = await _storage.read(key: _accessTokenKey);
+    final refreshToken = await _storage.read(key: _refreshTokenKey);
+
     final url = Uri.parse(AppUrl.homeVideosUrl).replace(queryParameters: {
       'page': homeVideosRequest.page.toString(),
       'size': homeVideosRequest.size.toString(),
@@ -17,6 +25,8 @@ class HomeRepository {
 
     final headers = {
       'Content-Type': 'application/json;charset=UTF-8',
+      if (accessToken != null && refreshToken != null)
+        "cookie": "x-access-token=$accessToken;x-refresh-token=$refreshToken"
     };
 
     final response = await http.get(url, headers: headers);
