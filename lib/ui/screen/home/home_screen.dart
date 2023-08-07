202f5c00ecc9927e29647ddec328bd7c1863c565
minWachya <minWachya@gmail.com>
2023-08-07 19:36:32 +0900

    :sparkles: [feat] 미로그인 시 스테이지/채팅/업로드 화면 입장 불가 로직 추가

--- lib/ui/screen/home/home_screen.dart
+++ lib/ui/screen/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:pocket_pose/data/local/provider/video_play_provider.dart';
+import 'package:pocket_pose/data/remote/provider/kakao_login_provider.dart';
 
 import 'package:pocket_pose/ui/screen/chat/chat_list_screen.dart';
 
@@ -19,11 +20,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late VideoPlayProvider _videoPlayProvider;
+  late KaKaoLoginProvider _loginProvider;
 
   @override
   void initState() {
     super.initState();
     _videoPlayProvider = Provider.of<VideoPlayProvider>(context, listen: false);
+    _loginProvider = Provider.of<KaKaoLoginProvider>(context, listen: false);
+    _loginProvider.mainContext = context;
   }
 
   @override
@@ -47,12 +51,13 @@ class _HomeScreenState extends State<HomeScreen> {
           elevation: 0.0, //appBar 그림자 농도 설정 (값 0으로 제거)
           actions: [
             GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ChatListScreen()),
-                  );
+                onTap: () async {
+                  if (await _loginProvider.checkAccessToken()) {
+                    _videoPlayProvider.pauseVideo();
+                    _showChatScreen();
+                  } else {
+                    _loginProvider.showLoginBottomSheet();
+                  }
                 },
                 child: SvgPicture.asset('assets/icons/ic_home_chat_list.svg')),
             UploadButtonWidget(
@@ -72,5 +77,12 @@ class _HomeScreenState extends State<HomeScreen> {
         extendBodyBehindAppBar: true, //body 위에 appbar
         resizeToAvoidBottomInset: false,
         body: const VideoView(screenName: 'home'));
+  }
+
+  void _showChatScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const ChatListScreen()),
+    );
   }
 }
