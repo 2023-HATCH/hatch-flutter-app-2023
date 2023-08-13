9f181c979f12f2afc41c38641c4304bd22789ce0
devMooon <anstjdus0702@duksung.ac.kr>
2023-08-13 23:31:24 +0900

    :bug: [bug] 로그인 바텀시트의 height 고정 (HH-218)

--- lib/ui/widget/login_modal_content_widget.dart
+++ lib/ui/widget/login_modal_content_widget.dart
@@ -17,60 +17,63 @@ class _LoginModalContentState extends State<LoginModalContent> {
   Widget build(BuildContext context) {
     _loginProvider = Provider.of<KaKaoLoginProvider>(context);
 
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            InkWell(
-              onTap: () {
-                //bottom sheet 없애기
-                Navigator.pop(context);
-              },
-              child: Container(
-                margin: const EdgeInsets.fromLTRB(18, 18, 0, 0),
-                child: SvgPicture.asset(
-                  'assets/icons/ic_exit.svg',
-                  width: 14,
+    return SizedBox(
+      height: 400,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              InkWell(
+                onTap: () {
+                  //bottom sheet 없애기
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(18, 18, 0, 0),
+                  child: SvgPicture.asset(
+                    'assets/icons/ic_exit.svg',
+                    width: 14,
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 14, 0, 14),
-              child: const Text(
-                "Pocket Pose 가입하기",
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 14, 0, 14),
+                child: const Text(
+                  "Pocket Pose 가입하기",
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 0, 0, 14),
-              child: const Text(
-                "간편하게 로그인하고 다양한 서비스를 이용해보세요.",
-                style: TextStyle(color: Colors.black87),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 14),
+                child: const Text(
+                  "간편하게 로그인하고 다양한 서비스를 이용해보세요.",
+                  style: TextStyle(color: Colors.black87),
+                ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 0, 0, 14),
-              child: SvgPicture.asset(
-                'assets/images/kakao_login_popo.svg',
-                width: 150,
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 14),
+                child: SvgPicture.asset(
+                  'assets/images/kakao_login_popo.svg',
+                  width: 150,
+                ),
               ),
-            ),
-            _loginButton(
-              'kakao_login',
-              () {
-                _loginProvider.signIn();
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ],
+              _loginButton(
+                'kakao_login',
+                () {
+                  _loginProvider.signIn();
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
