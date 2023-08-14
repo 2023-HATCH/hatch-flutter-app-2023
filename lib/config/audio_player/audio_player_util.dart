d2750ae261982ea941a38262e456ce210629ea32
minWachya <minWachya@gmail.com>
2023-08-14 21:40:59 +0900

    :bug: [fix] 스테이지 audio player 오류 부분 주석처리

--- lib/config/audio_player/audio_player_util.dart
+++ lib/config/audio_player/audio_player_util.dart
@@ -1,10 +1,9 @@
-import 'package:audio_session/audio_session.dart';
 import 'package:camera/camera.dart';
 import 'package:just_audio/just_audio.dart';
 
 class AudioPlayerUtil {
   CameraController? _controller;
-  late AudioSession audioSession;
+  // late AudioSession? audioSession;
   AudioPlayer player = AudioPlayer();
 
   static final AudioPlayerUtil _instance = AudioPlayerUtil._internal();
@@ -12,7 +11,7 @@ class AudioPlayerUtil {
   factory AudioPlayerUtil() => _instance;
 
   AudioPlayerUtil._internal() {
-    _audioSessionConfigure();
+    // _audioSessionConfigure();
   }
 
   setCameraController(CameraController? cameraController) {
@@ -27,7 +26,7 @@ class AudioPlayerUtil {
     // 내부 음악 실행
     await player.play();
     // 외부 음악 종료
-    // await audioSession.setActive(false);
+    // await audioSession?.setActive(false);
   }
 
   playSeek(int sec) async {
@@ -35,7 +34,7 @@ class AudioPlayerUtil {
     await player.seek(Duration(seconds: sec));
     await player.play();
     // 외부 음악 종료
-    // await audioSession.setActive(false);
+    // await audioSession?.setActive(false);
   }
 
   stop() async {
@@ -49,27 +48,27 @@ class AudioPlayerUtil {
     // 내부 음악 종료
     await player.stop();
     // 외부 음악 실행
-    // await audioSession.setActive(true);
+    // await audioSession?.setActive(true);
   }
 
   // 외부 음악 들릴 때 반응 설정
-  _audioSessionConfigure() =>
-      AudioSession.instance.then((audioSession) async => await audioSession
-          .configure(const AudioSessionConfiguration(
-              avAudioSessionCategory: AVAudioSessionCategory.playback,
-              avAudioSessionCategoryOptions: AVAudioSessionCategoryOptions.none,
-              avAudioSessionMode: AVAudioSessionMode.defaultMode,
-              avAudioSessionRouteSharingPolicy:
-                  AVAudioSessionRouteSharingPolicy.defaultPolicy,
-              avAudioSessionSetActiveOptions:
-                  AVAudioSessionSetActiveOptions.notifyOthersOnDeactivation,
-              androidAudioAttributes: AndroidAudioAttributes(
-                contentType: AndroidAudioContentType.music,
-                flags: AndroidAudioFlags.none,
-                usage: AndroidAudioUsage.media,
-              ),
-              androidAudioFocusGainType:
-                  AndroidAudioFocusGainType.gainTransient,
-              androidWillPauseWhenDucked: true))
-          .then((_) => audioSession = audioSession));
+  // _audioSessionConfigure() =>
+  //     AudioSession.instance.then((audioSession) async => await audioSession
+  //         .configure(const AudioSessionConfiguration(
+  //             avAudioSessionCategory: AVAudioSessionCategory.playback,
+  //             avAudioSessionCategoryOptions: AVAudioSessionCategoryOptions.none,
+  //             avAudioSessionMode: AVAudioSessionMode.defaultMode,
+  //             avAudioSessionRouteSharingPolicy:
+  //                 AVAudioSessionRouteSharingPolicy.defaultPolicy,
+  //             avAudioSessionSetActiveOptions:
+  //                 AVAudioSessionSetActiveOptions.notifyOthersOnDeactivation,
+  //             androidAudioAttributes: AndroidAudioAttributes(
+  //               contentType: AndroidAudioContentType.music,
+  //               flags: AndroidAudioFlags.none,
+  //               usage: AndroidAudioUsage.media,
+  //             ),
+  //             androidAudioFocusGainType:
+  //                 AndroidAudioFocusGainType.gainTransient,
+  //             androidWillPauseWhenDucked: true))
+  //         .then((_) => audioSession = audioSession));
 }
