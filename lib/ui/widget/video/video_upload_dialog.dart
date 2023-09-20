7d4032356bd1017d84cf2003ebe7fd950438a4c9
devMooon <anstjdus0702@duksung.ac.kr>
2023-09-20 21:07:29 +0900

    :package: [pakage] problems 해결

--- lib/ui/widget/video/video_upload_dialog.dart
+++ lib/ui/widget/video/video_upload_dialog.dart
@@ -20,6 +20,7 @@ class VideoUploadDialog extends StatefulWidget {
   }) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _VideoUploadDialogState createState() => _VideoUploadDialogState();
 }
 
