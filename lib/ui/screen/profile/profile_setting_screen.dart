c808a9fa2c76492c1b3da0b762f1f78babf441c9
devMooon <anstjdus0702@duksung.ac.kr>
2023-09-25 15:02:54 +0900

    :recycle: [refactor] 프로필 설정 screen 리팩토링 (HH-453)

--- lib/ui/screen/profile/profile_setting_screen.dart
+++ lib/ui/screen/profile/profile_setting_screen.dart
@@ -17,6 +17,7 @@ class ProfileSettingScreen extends StatefulWidget {
 class _ProfileSettingScreenState extends State<ProfileSettingScreen> {
   late KaKaoLoginProvider _loginProvider;
   late MultiVideoPlayProvider _multiVideoPlayProvider;
+
   @override
   void initState() {
     super.initState();
@@ -30,115 +31,97 @@ class _ProfileSettingScreenState extends State<ProfileSettingScreen> {
     return GestureDetector(
       onHorizontalDragUpdate: (details) {
         if (details.primaryDelta! > 10) {
-          // 왼쪽에서 오른쪽으로 드래그했을 때 pop
           Navigator.of(context).pop();
         }
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            '설정 및 개인정보',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back_ios_new_rounded,
-              color: AppColor.purpleColor,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          elevation: 0,
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(2.0),
-            child: Container(
-              height: 2.0,
-              color: AppColor.purpleColor,
-            ),
-          ),
-        ),
-        body: Column(children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(25, 30, 25, 30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    SvgPicture.asset(
-                        'assets/icons/ic_profile_setting_privacy_greement.svg'),
-                    const Padding(padding: EdgeInsets.fromLTRB(0, 0, 18, 0)),
-                    const Text(
-                      '개인정보 동의서',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
-                ),
-                Image.asset('assets/icons/ic_profile_setting_next.png'),
-              ],
-            ),
-          ),
-          GestureDetector(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(25, 0, 25, 30),
-              child: Row(
-                children: [
-                  SvgPicture.asset(
-                      'assets/icons/ic_profile_setting_logout.svg'),
-                  const Padding(padding: EdgeInsets.fromLTRB(0, 0, 18, 0)),
-                  const Text(
-                    '로그아웃',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
-            onTap: () => {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return CustomSimpleDialog(
-                      title: '⚠️ 로그아웃',
-                      message: '정말 로그아웃 하시겠습니까?',
-                      onCancel: () {
-                        Navigator.pop(context);
-                      },
-                      onConfirm: () {
-                        _loginProvider.signOut();
-
-                        _multiVideoPlayProvider.resetVideoPlayer(0);
-
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const MainScreen()),
-                          (route) => false,
-                        );
-                      },
-                    );
-                  })
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(25, 0, 25, 30),
-            child: Row(
-              children: [
-                SvgPicture.asset(
-                    'assets/icons/ic_profile_setting_withdrawal.svg'),
-                const Padding(padding: EdgeInsets.fromLTRB(0, 0, 18, 0)),
-                const Text(
-                  '회원탈퇴',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-          ),
-        ]),
+        appBar: _appbar(context),
+        body: _body(context),
       ),
     );
+  }
+
+  AppBar _appbar(BuildContext context) {
+    return AppBar(
+      title: const Text(
+        '설정 및 개인정보',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back_ios_new_rounded,
+          color: AppColor.purpleColor,
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      elevation: 0,
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(2.0),
+        child: Container(
+          height: 2.0,
+          color: AppColor.purpleColor,
+        ),
+      ),
+    );
+  }
+
+  Column _body(BuildContext context) {
+    return Column(children: [
+      const SizedBox(height: 10),
+      _row("privacy_greement", "개인정보 동의서", null),
+      _row("logout", "로그아웃", showLogoutDialog),
+      _row("withdrawal", "회원탈퇴", null)
+    ]);
+  }
+
+  InkWell _row(String image, String title, Function? ontap) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(25, 15, 25, 15),
+        child: Row(
+          children: [
+            SvgPicture.asset('assets/icons/ic_profile_setting_$image.svg'),
+            const Padding(padding: EdgeInsets.fromLTRB(0, 0, 18, 0)),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+      onTap: () => {
+        if (ontap != null) {ontap()}
+      },
+    );
+  }
+
+  Future<dynamic> showLogoutDialog() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return CustomSimpleDialog(
+            title: '⚠️ 로그아웃',
+            message: '정말 로그아웃 하시겠습니까?',
+            onCancel: () {
+              Navigator.pop(context);
+            },
+            onConfirm: () {
+              _loginProvider.signOut();
+
+              _multiVideoPlayProvider.resetVideoPlayer(0);
+
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const MainScreen()),
+                (route) => false,
+              );
+            },
+          );
+        });
   }
 }
